ced0db70263710717f72bc4219c4b9f94a2ad352
Brian Gunn <github@gunnbr.org>
2021-06-04 10:20:55 -0700

    Fix invalid factoid check to only consider whole words. Fixes #5

--- Gthx.Bot/Modules/FactoidModule.cs
+++ Gthx.Bot/Modules/FactoidModule.cs
@@ -10,7 +10,7 @@ namespace Gthx.Bot.Modules
     public class FactoidModule : IGthxModule
     {
         private readonly Regex _FactoidSet = new Regex(@"(?'factoid'.+?)\s(?'isAre'is|are)(?'hasAlso'\salso)?\s(?'value'.+)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
-        private readonly Regex _InvalidRegex = new Regex(@"(here|how|it|something|that|this|what|when|where|which|who|why|you)", RegexOptions.IgnoreCase);
+        private readonly Regex _InvalidRegex = new Regex(@"^(here|how|it|something|that|this|what|when|where|which|who|why|you)$", RegexOptions.IgnoreCase);
         private readonly Regex _FactoidGet = new Regex(@$"(?'factoid'.+)[?!](?'hasPipe'\s*$|\s*\|\s*(?'pipeToUser'{GthxUtil.NickMatch})$)", RegexOptions.Compiled);
         private readonly IGthxData _Data;
         private readonly IIrcClient _IrcClient;
